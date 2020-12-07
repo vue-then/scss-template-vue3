16e60b60d08f05e98d322599c34533663f454247
yyccQQu <643929860@qq.com>
2020-12-07 23:16:54 +0400

    init vue3 1

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import datav from 'datav-libs-dev'
+
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(datav)
+  .mount('#app')
